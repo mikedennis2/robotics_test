b63066c40495f280ddc070131542da123d93f49d
mikedennis2 <mikeadennis@gmail.com>
2020-10-26 22:35:48 +0000

    Test repo connection

--- src/main/java/org/eclipse/che/examples/HelloWorld.java
+++ src/main/java/org/eclipse/che/examples/HelloWorld.java
@@ -6,7 +6,20 @@ package org.eclipse.che.examples;
 
 public class HelloWorld {
     public static void main(String... argvs) {
-        String a = "Che";
-        System.out.println("Hello World " + a + "!");
+        String student_a_name = "Mike";
+        int student_a_age = 25;
+        int student_a_credit_hours = 15;
+        double rate_per_credit_hour = 550.75;
+
+        double student_a_fees = student_a_credit_hours * rate_per_credit_hour;
+        System.out.println("Student a's name is: " + student_a_name + " and he is " + student_a_age + ".");
+        System.out.println("Student a's total cost of attendance is: $" + student_a_fees + ".");
+
+        // Define another student 
+
+        // What is inefficient here?
+
+        // Solve the inefficiency below
+
     }
 }
